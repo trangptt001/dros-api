9d544faa5fd80bc7475cf0a5eac20a47be5399d2
TRANGPTT\Administrator <trangptt@vnext.com.vn>
2021-10-19 22:58:42 +0700

    env

--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService, UserService],
+  providers: [AuthService, UserService, JwtService],
   controllers: [AuthController]
 })
 export class AuthModule {}
